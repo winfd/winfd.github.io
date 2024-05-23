7178913e21e400d18533f98152da04a986b0982b
meshpotato1337 <167641336+winfd@users.noreply.github.com>
2024-05-23 11:05:06 +0300

    test emoji #6

--- _plugins/jekyll-emoji.rb
+++ _plugins/jekyll-emoji.rb
@@ -4,17 +4,17 @@ module Jekyll
 
   module EmojiFilter
 
-    def emojify(content)
+    def smile(content)
       return false if !content
 
       config = @context.registers[:site].config
-      if config['emoji_dir']
-        emoji_dir = config['emoji_dir']
+      if config['emoji_path']
+        emoji_path = config['emoji_path']
       end
 
       content.to_str.gsub(/:([a-z0-9\+\-_]+):/) do |match|
-        if Emoji.find_by_alias($1) and emoji_dir
-          '<img alt="' + $1 + '" src="' + emoji_dir + "/#{$1}.png" + '" class="emoji" />'
+        if Emoji.find_by_alias($1) and emoji_path
+          '<img alt="' + $1 + '" src="' + emoji_path + "/#{$1}.gif" + '" class="emoji" />'
         else
           match
         end
@@ -26,21 +26,21 @@ module Jekyll
   class EmojiGenerator < Generator
     def generate(site)
       config = site.config
-      return false if not config['emoji_dir']
-      return false if config['emoji_dir'].start_with?('http')
-      emoji_dir = File.join(config['source'], config['emoji_dir'])
-      return false if File.exist?(File.join(emoji_dir, 'smiley.png'))
+      return false if not config['emoji_path']
+      return false if config['emoji_path'].start_with?('http')
+      emoji_path = File.join(config['source'], config['emoji_path'])
+      return false if File.exist?(File.join(emoji_path, 'sixsense.gif'))
 
-      puts "           Copying: Emoji from Gemoji to " + config['emoji_dir']
+      puts "           Copying: Emoji from Gemoji to " + config['emoji_path']
 
       # Make Emoji directory
-      FileUtils.mkdir_p(emoji_dir)
+      FileUtils.mkdir_p(emoji_path)
 
       # Copy Gemoji files
-      unicode_emoji_dir = File.join(Emoji.images_path, 'emoji')
+      unicode_emoji_path = File.join(Emoji.images_path, 'emoji')
       Emoji.all.each do |em|
         # Use the name rather than the unicode character
-        FileUtils.cp File.join(unicode_emoji_dir, em.image_filename), File.join(emoji_dir, em.name + '.png')
+        FileUtils.cp File.join(unicode_emoji_path, em.image_filename), File.join(emoji_path, em.name + '.png')
       end
     end
   end
